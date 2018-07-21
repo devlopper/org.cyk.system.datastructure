9684b37ac7a7f541bdcab28be505967b01d74454
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-07-21 00:07:16 +0000

    Change-Id: Iafec869d70dfc69ada9a61576df0938602a523db

---
+++ datastructure-server-persistence-impl/src/main/java/org/cyk/system/datastructure/server/persistence/impl/collection/set/nested/NestedSetNodePersistenceImpl.java
@@ -0,0 +1,12 @@
+package org.cyk.system.datastructure.server.persistence.impl.collection.set.nested;
+
+import java.io.Serializable;
+
+import org.cyk.system.datastructure.server.persistence.api.collection.set.nested.NestedSetNodePersistence;
+import org.cyk.system.datastructure.server.persistence.entities.collection.set.nested.NestedSetNode;
+import org.cyk.utility.server.persistence.AbstractPersistenceEntityImpl;
+
+public class NestedSetNodePersistenceImpl extends AbstractPersistenceEntityImpl<NestedSetNode> implements NestedSetNodePersistence, Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
--- datastructure-server-persistence-impl/src/test/java/org/cyk/system/datastructure/server/persistence/impl/integration/NestedSetNodePersistenceIntegrationTest.java
+++ datastructure-server-persistence-impl/src/test/java/org/cyk/system/datastructure/server/persistence/impl/integration/NestedSetNodePersistenceIntegrationTest.java
@@ -1,74 +1,23 @@
 package org.cyk.system.datastructure.server.persistence.impl.integration;
 
+import javax.inject.Inject;
+
+import org.cyk.system.datastructure.server.persistence.api.collection.set.nested.NestedSetPersistence;
 import org.cyk.system.datastructure.server.persistence.entities.collection.set.nested.NestedSet;
 import org.cyk.system.datastructure.server.persistence.entities.collection.set.nested.NestedSetNode;
-import org.cyk.utility.server.persistence.test.TestPersistenceCreate;
-import org.cyk.utility.server.persistence.test.arquillian.AbstractArquillianIntegrationTestWithDefaultDeploymentAsSwram;
-import org.junit.Test;
+import org.cyk.utility.server.persistence.test.arquillian.AbstractPersistenceEntityIntegrationTest;
 
-public class NestedSetNodePersistenceIntegrationTest extends AbstractArquillianIntegrationTestWithDefaultDeploymentAsSwram {
+public class NestedSetNodePersistenceIntegrationTest extends AbstractPersistenceEntityIntegrationTest<NestedSetNode> {
 	private static final long serialVersionUID = 1L;
-		
-	@Test
-	public void create() throws Exception{
-		String set01Code = getRandomCode();
-		NestedSet set01 = new NestedSet().setCode(set01Code);
-		__inject__(TestPersistenceCreate.class).setObject(set01).execute().assertThrowableIsNull();
-		String node01Code = getRandomCode();
-		NestedSetNode node01 = new NestedSetNode().setSet(set01).setCode(node01Code).setLeftIndex(0).setRightIndex(1);
-		__inject__(TestPersistenceCreate.class).setObject(node01).execute().assertThrowableIsNull();
-	}
-	/*
-	@Test
-	public void read() throws Exception{
-		String code = getRandomCode();
-		NestedSet entity = new NestedSet().setCode(code);
-		userTransaction.begin();
-		persistence.create(entity);
-		userTransaction.commit();
-		assertThat(entity.getIdentifier()).isNotNull();
-		entity = persistence.readOne(entity.getIdentifier());
-		assertThat(entity).isNotNull();
-		assertThat(entity.getCode()).isEqualTo(code);
-		assertionHelper.assertStartsWithLastLogEventMessage("Server Persistence Read NestedSet").assertContainsLastLogEventMessage("code="+code);
-		
-	}
 	
-	@Test
-	public void update() throws Exception{
-		String code = getRandomCode();
-		NestedSet entity = new NestedSet().setCode(code);
-		userTransaction.begin();
-		persistence.create(entity);
-		userTransaction.commit();
-		entity = persistence.readOne(entity.getIdentifier());
-		String newCode = getRandomCode();
-		entity.setCode(newCode);
-		userTransaction.begin();
-		persistence.update(entity);
-		userTransaction.commit();
-		assertionHelper.assertStartsWithLastLogEventMessage("Server Persistence Update NestedSet").assertContainsLastLogEventMessage("code="+newCode);
-		entity = persistence.readOne(entity.getIdentifier());
-		assertThat(entity).isNotNull();
-		assertThat(entity.getCode()).isEqualTo(newCode);
-	}
+	@Inject private NestedSetPersistence nestedSetPersistence;
 	
-	@Test
-	public void delete() throws Exception{
-		String code = getRandomCode();
-		NestedSet entity = new NestedSet().setCode(code);
-		userTransaction.begin();
-		persistence.create(entity);
-		userTransaction.commit();
-		entity = persistence.readOne(entity.getIdentifier());
-		assertThat(entity).isNotNull();
-		userTransaction.begin();
-		persistence.delete(entity);
-		userTransaction.commit();
-		assertionHelper.assertStartsWithLastLogEventMessage("Server Persistence Delete NestedSet").assertContainsLastLogEventMessage("code="+code);
-		entity = persistence.readOne(entity.getIdentifier());
-		assertThat(entity).isNull();
+	@Override
+	protected NestedSetNode __instanciateEntity__(Object action) throws Exception {
+		NestedSetNode nestedSetNode = super.__instanciateEntity__(action);
+		nestedSetNode.setSet(new NestedSet().setCode(getRandomCode())).setLeftIndex(0).setRightIndex(0);
+		create(nestedSetPersistence,nestedSetNode.getSet());
+		return nestedSetNode;
 	}
-	*/
 	
 }
