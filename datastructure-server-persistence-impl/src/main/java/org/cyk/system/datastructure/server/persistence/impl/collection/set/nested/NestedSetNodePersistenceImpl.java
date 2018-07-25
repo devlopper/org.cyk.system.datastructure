688fb396a0a24ecd29d2f46f5282c5c3e016ab19
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-07-25 22:47:25 +0000

    Change-Id: I5959a3dc01d1ff586b0226b0f31b280a3324b952

--- datastructure-server-persistence-impl/src/main/java/org/cyk/system/datastructure/server/persistence/impl/collection/set/nested/NestedSetNodePersistenceImpl.java
+++ datastructure-server-persistence-impl/src/main/java/org/cyk/system/datastructure/server/persistence/impl/collection/set/nested/NestedSetNodePersistenceImpl.java
@@ -9,6 +9,8 @@ import org.cyk.system.datastructure.server.persistence.api.collection.set.nested
 import org.cyk.system.datastructure.server.persistence.entities.collection.set.nested.NestedSet;
 import org.cyk.system.datastructure.server.persistence.entities.collection.set.nested.NestedSetNode;
 import org.cyk.utility.server.persistence.AbstractPersistenceEntityImpl;
+import org.cyk.utility.server.persistence.query.PersistenceQuery;
+import org.cyk.utility.server.persistence.query.PersistenceQueryRepository;
 
 @Singleton
 public class NestedSetNodePersistenceImpl extends AbstractPersistenceEntityImpl<NestedSetNode> implements NestedSetNodePersistence, Serializable {
@@ -25,24 +27,36 @@ public class NestedSetNodePersistenceImpl extends AbstractPersistenceEntityImpl<
 	
 	@Override
 	public Collection<NestedSetNode> readBySet(NestedSet set) {
-		return __readMany__(NestedSetNode.FIELD_NESTED_SET, set);
+		return __readMany__(__getQueryParameters__(set));
 	}
 	
 	@Override
 	public Long countBySet(NestedSet set) {
-		return __count__(NestedSetNode.FIELD_NESTED_SET, set);
+		return __count__(__getQueryParameters__(set));
 	}
 	
 	@Override
 	public Collection<NestedSetNode> readByParent(NestedSetNode node) {
-		return __readMany__(NestedSetNode.FIELD_NESTED_SET, node.getNestedSet(),NestedSetNode.FIELD_LEFT_INDEX,node.getLeftIndex(),NestedSetNode.FIELD_RIGHT_INDEX
-				,node.getRightIndex());
+		return __readMany__(__getQueryParameters__(node));
 	}
 	
 	@Override
 	public Long countByParent(NestedSetNode node) {
-		return __count__(NestedSetNode.FIELD_NESTED_SET, node.getNestedSet(),NestedSetNode.FIELD_LEFT_INDEX,node.getLeftIndex(),NestedSetNode.FIELD_RIGHT_INDEX
-				,node.getRightIndex());
+		return __count__(__getQueryParameters__(node));
 	}
 	
+	protected Object[] __getQueryParameters__(String queryIdentifier,Object...objects){
+		PersistenceQuery persistenceQuery = __inject__(PersistenceQueryRepository.class).getBySystemIdentifier(queryIdentifier);
+		
+		if(persistenceQuery.isIdentifierEqualsToOrQueryDerivedFromQueryIdentifierEqualsTo(readBySet,queryIdentifier))
+			return new Object[]{NestedSetNode.FIELD_NESTED_SET,objects[0]};
+		
+		if(persistenceQuery.isIdentifierEqualsToOrQueryDerivedFromQueryIdentifierEqualsTo(readByParent,queryIdentifier)){
+			NestedSetNode node = (NestedSetNode) objects[0];
+			return new Object[]{NestedSetNode.FIELD_NESTED_SET, node.getNestedSet(),NestedSetNode.FIELD_LEFT_INDEX,node.getLeftIndex(),NestedSetNode.FIELD_RIGHT_INDEX
+					,node.getRightIndex()};
+		}
+		
+		return super.__getQueryParameters__(queryIdentifier, objects);
+	}
 }
